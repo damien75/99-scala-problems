79f83d6cb54bf28cf6c4dbfe5fa288e11e096fd3
Damien GOBLOT <damiengoblot@gmail.com>
2017-04-16 21:26:14 -0400

    P02: Testing on lists of strings

--- src/test/scala/p01/LastElementSuite.scala
+++ src/test/scala/p01/LastElementSuite.scala
@@ -6,8 +6,12 @@ import org.scalatest.FunSuite
   * Created by damiengoblot on 2017-04-16.
   */
 class LastElementSuite extends FunSuite{
-  test("last: list"){
+  test("last: list of ints"){
     assert(new LastElement().last(List(0, 1, 2, 3, 8, 5)) === 5)
+  }
+
+  test("last: list of strings"){
+    assert(new LastElement().last(List('aasdf', 'awesrfv', 'sfgh', 'sdfr', 'qer', 'ktyufj')) === 'ktyufj')
   }
 
   test("last: empty list"){
