8ec8645b32700e8b73564763b1aaaa18957c633d
Damien GOBLOT <damiengoblot@gmail.com>
2017-04-16 22:36:54 -0400

    P02: Last but one element in list

---
+++ src/main/scala/p01/SecondToLastElement.scala
@@ -0,0 +1,15 @@
+package p01
+
+import java.util.NoSuchElementException
+
+/**
+  * Created by damiengoblot on 2017-04-16.
+  */
+class SecondToLastElement {
+  @scala.annotation.tailrec
+  final def secondToLast[A](list: List[A]): A = list match {
+    case s_to_last :: _ :: Nil => s_to_last
+    case _ :: tail => secondToLast(tail)
+    case _ => throw new NoSuchElementException
+  }
+}
